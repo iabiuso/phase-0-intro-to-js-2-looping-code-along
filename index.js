b1e8203acf6468e15a0980264ff00e9eca02a7b5
iabiuso <ignacioabiuso@gmail.com>
2021-03-19 08:47:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solutions in this file
+
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+const eventName = "surprise";
+
+const messagesArray = [];
+
+function writeCards(names,eventName) {
+    for (let i = 0; i < names.length; i++) {
+        //console.log(`"Thank you, ${names[i]} for the wonderful surprise gift!"`);
+        //debugger;
+
+    
+        messagesArray.push("Thank you, " + names[i] + ", for the wonderful " + eventName + " gift!");
+        
+
+    }
+    
+    return messagesArray;
+}
+
+//Collect the messages in an array and return this array
+
+// -------- while -------------------------------
+
+function countDown(number){
+    let i = 0;
+    while (number >= 0){
+        i++;
+        console.log(number);
+        number--;
+    
+    
+    }
+    return i;
+
+}
+
+
+
+console.log(countDown(10));
+console.log(countDown(4));
+
+
